1e96c2958a733a0067fb3761add4d5f86058a87a
JimBobSquarePants <james_south@hotmail.com>
2017-08-28 17:18:05 +1000

    Add Nuspec

--- src/UmbMapper.PublishedContentModelFactory/Properties/AssemblyInfo.cs
+++ src/UmbMapper.PublishedContentModelFactory/Properties/AssemblyInfo.cs
@@ -1,14 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// <copyright file="AssemblyInfo.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UmbMapper.PublishedContentModelFactory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extensions for UmbMapper to allow the mapping of IPublishedContent based classes using the IPublishedContentModelFactory")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("James Jackson-South")]
 [assembly: AssemblyProduct("UmbMapper.PublishedContentModelFactory")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
