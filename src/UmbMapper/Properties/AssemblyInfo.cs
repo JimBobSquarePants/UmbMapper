e023fea71ab46e445a0f4735f789c8a1fa5fdbe6
Euan Rae <euan37@hotmail.com>
2019-06-19 15:49:24 +0100

    setup test project, update internals visible for Umbraco 8 test project

--- src/UmbMapper/Properties/AssemblyInfo.cs
+++ src/UmbMapper/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: InternalsVisibleTo("Umbraco.Tests")]
+[assembly: InternalsVisibleTo("UmbMapper.Umbraco8.Tests")]
---
+++ tests/UmbMapper.Umbraco8.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ArrayExtensionTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+using UmbMapper.Extensions;
+using Xunit;
+
+namespace UmbMapper.Umbraco8.Tests
+{
+    public class ArrayExtensionTests
+    {
+        [Fact]
+        public void ArrayCanRemoveItemAtIndex()
+        {
+            int[] input = new int[] { 1, 2, 3, 4, 5 };
+            int[] expected = new int[] { 1, 2, 4, 5 };
+            int[] actual = input.RemoveAt(2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
