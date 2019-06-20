4b5680feeb12d92b456b4f9b42ae33f12c36f8a0
Euan Rae <euan37@hotmail.com>
2019-06-20 11:04:04 +0100

    adding enum tests

---
+++ tests/UmbMapper.Umbraco8.Tests/Mapping/EnumMappingTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UmbMapper.Umbraco8.Tests.Mapping.Models;
+using UmbMapper.Umbraco8.Tests.Mocks;
+using UmbMapper.Extensions;
+using Xunit;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace UmbMapper.Umbraco8.Tests.Mapping
+{
+    public class EnumMappingTests : IClassFixture<UmbracoSupport>
+    {
+        private readonly UmbracoSupport support;
+
+        public EnumMappingTests(UmbracoSupport support)
+        {
+            this.support = support;
+        }
+
+        [Fact]
+        public void MapperReturnsDefaultEnum()
+        {
+            const PlaceOrder placeOrder = PlaceOrder.Fourth;
+
+            MockPublishedContent content = this.support.Content;
+            content.Properties = new List<IPublishedProperty>
+            {
+                MockHelper.CreateMockPublishedProperty(nameof(PublishedItem.PlaceOrder), null)
+            };
+
+            PublishedItem result = content.MapTo<PublishedItem>();
+
+            Assert.NotEqual(placeOrder, result.PlaceOrder);
+            Assert.Equal(default(PlaceOrder), result.PlaceOrder);
+        }
+
+        [Fact]
+        public void MapperReturnsCorrectEnumFromInt()
+        {
+            const PlaceOrder placeOrder = PlaceOrder.Fourth;
+
+            MockPublishedContent content = this.support.Content;
+            content.Properties = new List<IPublishedProperty>
+            {
+                MockHelper.CreateMockPublishedProperty(nameof(PublishedItem.PlaceOrder), (int)placeOrder)
+            };
+
+            PublishedItem result = content.MapTo<PublishedItem>();
+
+            Assert.Equal(placeOrder, result.PlaceOrder);
+        }
+
+        [Fact]
+        public void MapperReturnsCorrectEnumFromString()
+        {
+            const PlaceOrder placeOrder = PlaceOrder.Fourth;
+
+            MockPublishedContent content = this.support.Content;
+            content.Properties = new List<IPublishedProperty>
+            {
+                MockHelper.CreateMockPublishedProperty(nameof(PublishedItem.PlaceOrder), placeOrder.ToString())
+            };
+
+            PublishedItem result = content.MapTo<PublishedItem>();
+
+            Assert.Equal(placeOrder, result.PlaceOrder);
+        }
+    }
+}
