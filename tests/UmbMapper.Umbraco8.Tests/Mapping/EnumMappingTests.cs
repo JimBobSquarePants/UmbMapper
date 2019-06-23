3f25944d75227e8b495b7f9f590c851ada042745
Euan Rae <euan37@hotmail.com>
2019-06-23 15:07:22 +0100

    starting to try and instantiate an UmbracoContext for testing

--- tests/UmbMapper.Umbraco8.Tests/Mapping/EnumMappingTests.cs
+++ tests/UmbMapper.Umbraco8.Tests/Mapping/EnumMappingTests.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UmbMapper.Umbraco8.Tests.Mapping.Models;
 using UmbMapper.Umbraco8.Tests.Mocks;
 using UmbMapper.Extensions;
 using Xunit;
 using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using System.Reflection;
+using Moq;
+using Umbraco.Tests.TestHelpers;
+using Umbraco.Core.Configuration.UmbracoSettings;
+using Umbraco.Web.PublishedCache;
+using Umbraco.Core.Configuration;
+using Umbraco.Web.Security;
+using Umbraco.Core.Services;
+using Umbraco.Web.Routing;
+using Umbraco.Tests.Testing.Objects.Accessors;
 
 namespace UmbMapper.Umbraco8.Tests.Mapping
 {
@@ -63,6 +75,39 @@ namespace UmbMapper.Umbraco8.Tests.Mapping
             PublishedItem result = content.MapTo<PublishedItem>();
 
             Assert.Equal(placeOrder, result.PlaceOrder);
+        }
+
+        [Fact]
+        public void UmbracoContextNotNull()
+        {
+            // Get the internal constructor
+            var constructor = typeof(UmbracoContext)
+                .GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic).First();
+
+            var ctorParams = constructor.GetParameters();
+
+            // build required parameters
+            var _httpContextFactory = new FakeHttpContextFactory("~/Home");
+            var umbracoSettings = Mock.Of<IUmbracoSettingsSection>();
+            var globalSettings = Mock.Of<IGlobalSettings>();
+            var publishedSnapshotService = new Mock<IPublishedSnapshotService>();
+            publishedSnapshotService.Setup(x => x.CreatePublishedSnapshot(It.IsAny<string>())).Returns(Mock.Of<IPublishedSnapshot>());
+            var ctxMock = new Mock<UmbracoContext>();
+
+            var instance =
+                constructor.Invoke(
+                    new object[] {
+                        _httpContextFactory.HttpContext,
+                        publishedSnapshotService.Object,
+                        new WebSecurity(_httpContextFactory.HttpContext, Mock.Of<IUserService>(), globalSettings),
+                        umbracoSettings,
+                        Enumerable.Empty<IUrlProvider>(),
+                        globalSettings,
+                        new TestVariationContextAccessor()
+                    }
+                );
+
+
         }
     }
 }
