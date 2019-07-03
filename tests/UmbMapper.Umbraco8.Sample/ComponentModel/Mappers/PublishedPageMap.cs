b64d4b61cf6ce160b7cbf553bedc8d8f8e679f15
EuanRae <e.rae@ry.com>
2019-07-03 17:36:31 +0100

    adding all models/components/etc

---
+++ tests/UmbMapper.Umbraco8.Sample/ComponentModel/Mappers/PublishedPageMap.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="PublishedPageMap.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+using UmbMapper.Umbraco8.Sample.Models.Pages;
+
+namespace UmbMapper.Umbraco8.Sample.ComponentModel.Mappers
+{
+    /// <summary>
+    /// Configures mapping for basic page properties
+    /// </summary>
+    /// <typeparam name="T">The type of object to map.</typeparam>
+    public class PublishedPageMap<T> : UmbMapperConfig<T>
+        where T : PublishedPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublishedPageMap{T}"/> class.
+        /// </summary>
+        public PublishedPageMap()
+        {
+            // Map all items as lazy - This means we only map what properties you explicitly ask for in your calling code.
+            this.MapAll().ForEach(x => x.AsLazy());
+
+            // Update specific mappings with extra configuration
+            // this.AddMap(x => x.BrowserPageTitle).SetAlias(x => x.BrowserPageTitle, x => x.Name).SetMapper<VortoPropertyMapper>();
+            this.AddMap(x => x.OpenGraphImage).AsRecursive();
+        }
+    }
+}
--- tests/UmbMapper.Umbraco8.Sample/Composing/UmbMapperRegistryComponent.cs
+++ tests/UmbMapper.Umbraco8.Sample/Composing/UmbMapperRegistryComponent.cs
@@ -1,4 +1,6 @@
-﻿using UmbMapper.Umbraco8.Sample.Models.UmbracoDocTypes;
+﻿using UmbMapper.Umbraco8.Sample.ComponentModel.Mappers;
+using UmbMapper.Umbraco8.Sample.Models.Pages;
+using UmbMapper.Umbraco8.Sample.Models.UmbracoDocTypes;
 using Umbraco.Core.Composing;
 using Umbraco.Core.PropertyEditors.ValueConverters;
 
@@ -8,11 +10,21 @@ namespace UmbMapper.Umbraco8.Sample.Composing
     {
         public void Initialize()
         {
-            UmbMapperRegistry.AddMapperFor<SiteRoot>();
-            UmbMapperRegistry.AddMapperFor<ImageCropperValue>();
-            UmbMapperRegistry.AddMapperFor<MetaDataComposition>();
-            UmbMapperRegistry.AddMapperFor<BasicHeaderComposition>();
-            UmbMapperRegistry.AddMapperFor<BasicImage>();
+            //UmbMapperRegistry.AddMapperFor<SiteRoot>();
+            //UmbMapperRegistry.AddMapperFor<ImageCropperValue>();
+            //UmbMapperRegistry.AddMapperFor<MetaDataComposition>();
+            //UmbMapperRegistry.AddMapperFor<BasicHeaderComposition>();
+            //UmbMapperRegistry.AddMapperFor<BasicImage>();
+
+            // Register our custom maps
+            UmbMapperRegistry.AddMapper(new PublishedImageMap());
+            UmbMapperRegistry.AddMapper(new HomeMap());
+            UmbMapperRegistry.AddMapper(new BlogMap());
+            UmbMapperRegistry.AddMapper(new PostMap());
+
+            // The Slide document type is simple with no additional customization required.
+            // This can be mapped by convention. The mapper will implicitly lazy map any virtual properties.
+            UmbMapperRegistry.AddMapperFor<Slide>();
         }
 
         public void Terminate()
