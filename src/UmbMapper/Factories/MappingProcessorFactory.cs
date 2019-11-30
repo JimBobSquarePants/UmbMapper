ce8d47edde817bcabd86c347dea2b392d2c4c4bf
Euan Rae <euan37@hotmail.com>
2019-11-30 12:27:40 +0000

    adding some comments

--- src/UmbMapper/Factories/MappingProcessorFactory.cs
+++ src/UmbMapper/Factories/MappingProcessorFactory.cs
@@ -1,16 +1,34 @@
-﻿
-using System;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using Umbraco.Web;
 
 namespace UmbMapper.Factories
 {
+    /// <summary>
+    /// Factory to generate a mapping processor passing the the type to map to 
+    /// in the IUmbMapperConfig instance
+    /// </summary>
     public class MappingProcessorFactory : IMappingProcessorFactory
     {
         private readonly IUmbracoContextFactory umbracoContextFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingProcessorFactory"/> class.
+        /// Standard Constructor
+        /// </summary>
+        /// <param name="umbracoContextFactory"></param>
         public MappingProcessorFactory(IUmbracoContextFactory umbracoContextFactory)
         {
             this.umbracoContextFactory = umbracoContextFactory;
         }
+
+        /// <summary>
+        /// Main factory method to create an instance of <see cref="IMappingProcessor"/>
+        /// </summary>
+        /// <param name="config">IUmbMapperConfig instance that defines what's being mapped to</param>
+        /// <param name="umbMapperService">IUmbMapperService needed by the processor to do the mapping</param>
+        /// <returns>Instance of <see cref="IMappingProcessor"/></returns>
         public IMappingProcessor Create(IUmbMapperConfig config, IUmbMapperService umbMapperService)
         {
             Type genericType = typeof(MappingProcessor<>);
@@ -19,6 +37,10 @@ namespace UmbMapper.Factories
             Type constructedGenericType = genericType.MakeGenericType(typeArgs);
             object[] constructorArgs = new object[] { config, umbMapperService, this.umbracoContextFactory };
 
+            ConstructorInfo ctor = constructedGenericType.GetConstructors().First();
+
+            // The performance of this coudl be improved 
+            // ref: https://rogerjohansson.blog/2008/02/28/linq-expressions-creating-objects/
             return Activator.CreateInstance(constructedGenericType, constructorArgs) as IMappingProcessor;
         }
     }
